26ad5dfa71c38ad178bd3cd95bfa3bea84710bd2
Owais Abbasi <owais.abbasi9@gmail.com>
2022-03-03 18:38:55 +0500

    few lines added

--- testing r.R
+++ testing r.R
@@ -1,2 +1,8 @@
 print("This file was created within RStudio")
 print("And now it lives on GitHub")
+
+name = "Owais"
+Version = "Rstudio"
+
+print(name)
+print("These lines has been written in R Studio")
